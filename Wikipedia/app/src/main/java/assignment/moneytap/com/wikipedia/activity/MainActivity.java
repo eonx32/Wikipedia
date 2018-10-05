1610c5fcaacc85c01c43f931968d396a906a282b
Rajat Kumar <rajat.2k50@gmail.com>
2018-10-05 21:47:59 +0530

    Added basic code for app

---
+++ Wikipedia/app/src/main/java/assignment/moneytap/com/wikipedia/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package assignment.moneytap.com.wikipedia.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import assignment.moneytap.com.wikipedia.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
